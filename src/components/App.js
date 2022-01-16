107ac7134761350542c807522300ccdc0f1e7c11
Phil Than <82873669+pthan1@users.noreply.github.com>
2022-01-15 19:28:58 -0700

    Set article sections to state

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,13 @@ import DetailDisplay from './DetailDisplay';
 
 function App() {
   const [articles, setArticles] = useState([]);
+  const [sections, setSections] = useState([]);
+  const [filter, setFilter] = useState('');
   // const [displayArticle, setDisplayArticle] = useState({});
+   
+  
+
+
 
     useEffect(() => {
     fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=2ehzLoKkCXtF2lzzbaK2MCq8En9hZaNA`)
@@ -18,20 +24,38 @@ function App() {
   })
   .then(data => {
     const articleKeys = data.results.map((article, key) => {return { ...article, key: key }} );
-    setArticles(articleKeys)
+    const articleSelections = [];
+    articleKeys.forEach(article => {
+      if (!articleSelections.includes(article.section)) {
+        articleSelections.push(article.section);
+      }
+      });
+      setSections(articleSelections);
+      console.log(sections)
+
+  
+    setArticles(articleKeys);
     console.log(articleKeys)
-  })
+  });
+
+
+
+
   }, [])
+
+  const handleFilter = (value) => {
+
+  };
+
 
 
   return (
     <div className="landing-page">
       <h1>Daily Headlines</h1>
+      <select name="sections" id="sections" onChange={value => handleFilter(value)}></select><br />
       <div className="content">
         <ArticleContainer 
-        articles={articles} 
-        // setDisplayArticle={setDisplayArticle} 
-        // displayArticle={displayArticle}
+        articles={articles}
         />
       </div>
     </div>
